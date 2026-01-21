8885becab486250308adbd693bf1d79aa71cef59
zak-ari0 <zakariaaithadou334@gmail.com>
2026-01-21 16:52:50 +0100

    add responsive mobile

--- public/js/app.js
+++ public/js/app.js
@@ -33,15 +33,15 @@ setInterval(() => {
 }, 4000);
 
 
-const Slides = document.querySelectorAll(".event-slide");
-const Dots = document.querySelectorAll(".Dot");
-const track = document.querySelector(".eventsTrack");
+let Slides = document.querySelectorAll(".event-slide");
+let Dots = document.querySelectorAll(".Dot");
+let track = document.querySelector(".eventsTrack");
 
 let current = 0; 
-const visible = 3; 
+let visible = 3; 
 
 function update() {
-  const slideWidth = Slides[0].offsetWidth + 20; 
+  let slideWidth = Slides[0].offsetWidth + 20; 
   track.style.transform = `translateX(-${current * slideWidth}px)`;
 
   Dots.forEach(d => d.classList.remove("Active"));
@@ -68,5 +68,51 @@ setInterval(() => {
 
 update();
 
+
+
+
+
+let bookBtn = document.getElementById("bookBtn");
+let modal = document.getElementById("bookingModal");
+let closeBtn = document.querySelector(".close");
+let form = document.getElementById("bookingForm");
+let errorMsg = document.getElementById("errorMsg");
+
+let bookings = [];
+
+bookBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  errorMsg.textContent = "";
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+
+  let name = fullName.value.trim();
+  let mealType = meal.value;
+  let start = startTime.value;
+  let end = endTime.value;
+  let persons = people.value;
+
+  if (!start || !end) return;
+
+  let isBooked = bookings.some(b => b.start === start);
+
+  if (isBooked) {
+    errorMsg.textContent = " This time is already booked!";
+    return;
+  }
+
+  bookings.push({ name, mealType, start, end, persons });
+
+  alert(" Booking confirmed!");
+
+  modal.style.display = "none";
+  form.reset();
+});
 
 
